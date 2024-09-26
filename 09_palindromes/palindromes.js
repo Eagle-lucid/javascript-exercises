dd8709163c0843dc2fdcb2a43dd33af43250353b
Eagle-lucid <vicokefie@gmail.com>
2024-09-26 09:23:04 -0400

    09_palindromes/palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
 
+    // Step 2: Reverse the cleaned string
+    let reversedStr = cleanedStr.split('').reverse().join('');
+
+    // Step 3: Compare the cleaned string with its reversed version
+    return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
