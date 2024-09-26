05b01ea0cd33abcdc260587fc268cecf70b4914b
Eagle-lucid <vicokefie@gmail.com>
2024-09-26 10:02:45 -0400

    12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+   return people.reduce((oldest, person) => {
 
-};
+    const currentYear = new Date().getFullYear();
+    const deathYear = person.yearOfDeath || currentYear;
+
+    const personAge = deathYear - person.yearOfBirth;
+
+    const oldestDeathYear = oldest.yearOfDeath || currentYear;
+    const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+
+    return personAge > oldestAge ? person : oldest;
+   }
+
+
+)};
 
 // Do not edit below this line
 module.exports = findTheOldest;
